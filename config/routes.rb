2bdb0fa7282aad2e2d19c99d0b0640846da5b012
NikitaRybka <lubchenko2044@gmail.com>
2022-01-27 01:05:02 +0300

    Added theory page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resource :user do
-    resource :bitset
+    resource :bitset do
+      get '/theory', to: 'pages#theory'
+    end
   end
 end
